7e55573faeb48e16b13209a9bd6c6d0408a26468
Filipe Cândido <filipecandido123@hotmail.com>
2022-11-09 00:35:10 -0300

    feat: add news http helper, noContent and serverError

--- back-end/src/presentation/helpers/http/http-helper.ts
+++ back-end/src/presentation/helpers/http/http-helper.ts
@@ -1,6 +1,17 @@
+import { ServerError } from '../../errors/server-error'
 import { HttpResponse } from '../../protocols'
 
 export const OK = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
+})
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack as string)
 })
