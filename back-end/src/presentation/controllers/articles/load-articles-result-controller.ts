f40892924f0f0cf04aefb4cfffe0a8c9117f42dd
Filipe Cândido <filipecandido123@hotmail.com>
2022-11-09 15:55:42 -0300

    feat: Ensure LoadArticlesResult calls ArticleId where returns one article successfully

---
+++ back-end/src/presentation/controllers/articles/load-articles-result-controller.ts
@@ -0,0 +1,21 @@
+import { ArticleId } from '../../../domain/usecases/load-articles-result'
+import { InvalidParamError } from '../../errors/invalid-param-error'
+import { forbidden, OK, serverError } from '../../helpers/http/http-helper'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+
+export class LoadArticlesResult implements Controller {
+  constructor(private readonly articleId: ArticleId) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const id = httpRequest.params.id
+      const exists = await this.articleId.getIdArticle(id)
+      if (!exists) {
+        return forbidden(new InvalidParamError('articleId'))
+      }
+      return OK(exists)
+    } catch (error) {
+      return serverError(error as Error)
+    }
+  }
+}
