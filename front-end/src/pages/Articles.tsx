70d9224beba69b4fba6aed255731d5013219db7a
Filipe Cândido <filipecandido123@hotmail.com>
2022-11-09 18:04:41 -0300

    feat: add routes page articles

---
+++ front-end/src/pages/Articles.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Articles = (): JSX.Element => {
+  return (
+    <main>
+      <section>
+        <p>teste</p>
+      </section>
+    </main>
+  )
+}
+
+export default Articles
---
+++ front-end/src/routes/routes.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Articles from '../pages/Articles'
+
+const MainRoutes = () => {
+  return (
+    <Routes>
+      <Route element={<Articles />} />
+    </Routes>
+  )
+}
+
+export default MainRoutes
