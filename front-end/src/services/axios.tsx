af78314c931250a8c5ab5cfca2379f31d9d5db77
Filipe Cândido <filipecandido123@hotmail.com>
2022-11-09 21:36:15 -0300

    feat: change baseUrl axios

--- front-end/src/services/axios.tsx
+++ front-end/src/services/axios.tsx
@@ -1,5 +1,5 @@
-import axios from 'axios';
+import axios from 'axios'
 
-export const baseURL = 'http://localhost:5050/articles';
+export const baseURL = 'http://localhost:5050'
 
-export const api = axios.create({ baseURL });
+export const api = axios.create({ baseURL })
