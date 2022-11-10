83e3ec9f7e1aaea078bd2a27d0f4ba14cf14f180
Filipe Cândido <filipecandido123@hotmail.com>
2022-11-09 21:35:44 -0300

    feat: ensure Provider returns fetchApiArticles and handleNewArticlesApi render fetch api with correct values

--- front-end/src/provider/Provider.tsx
+++ front-end/src/provider/Provider.tsx
@@ -1,12 +1,49 @@
 import MyContext from '../context/MyContext'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { api } from '../services/axios'
+import { ArticlesModel } from '../interface/articles'
 
 interface Props {
   children: React.ReactNode
 }
 
 const Provider: React.FC<Props> = ({ children }) => {
-  const contextValue = {}
+  const [articlesData, setArticlesData] = useState<ArticlesModel[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+  const [hiddenButton, setHiddenButton] = useState<boolean>(true)
+
+  const fetchApiArticles = async (): Promise<void> => {
+    try {
+      setLoading(true)
+      const { data } = await api.get('/articles')
+      setArticlesData(data)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleNewArticlesApi = async (): Promise<void> => {
+    try {
+      setHiddenButton(true)
+      const { data } = await api.get('/articles?p=10')
+      setArticlesData(data)
+      setHiddenButton(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchApiArticles()
+  }, [])
+
+  const contextValue = {
+    articlesData,
+    loading,
+    handleNewArticlesApi,
+    hiddenButton,
+  }
 
   return <MyContext.Provider value={contextValue}>{children}</MyContext.Provider>
 }
